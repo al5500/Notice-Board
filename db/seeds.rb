42fb50a27aa106f7e09283bcad54d504781b9ebf
Alan Wright <alanwright@Alans-MacBook-Pro.local>
2015-06-02 14:08:51 +1000

    Ugh

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all
-u1 = User.create :name => 'Alan', :suburb => 'Terrey Hills', :email => 'awright5500@gmail.com', :phone => '0458824398', :image => 'http://fc01.deviantart.net/fs70/i/2013/078/2/b/finn_the_human__by_toastwaffleart-d5ylzg7.png'
+u1 = User.create :name => 'Alan', :suburb => 'Terrey Hills', :email => 'awright5500@gmail.com', :phone => '0458824398', :image => 'http://fc01.deviantart.net/fs70/i/2013/078/2/b/finn_the_human__by_toastwaffleart-d5ylzg7.png', :admin => true, :password => 'password'
